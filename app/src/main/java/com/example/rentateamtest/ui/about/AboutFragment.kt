b0073a7d123b25a4ef7ea6438b775bc303f31d66
AcerolaOrion598 <djapar-shah@mail.ru>
2021-07-17 13:15:45 +0300

    init

---
+++ app/src/main/java/com/example/rentateamtest/ui/about/AboutFragment.kt
@@ -0,0 +1,39 @@
+package com.example.rentateamtest.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.rentateamtest.R
+import com.example.rentateamtest.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textAbout: TextView = binding.textAbout
+        textAbout.text = getString(R.string.text_about)
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
